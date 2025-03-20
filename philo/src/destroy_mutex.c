577eafe7fbf94a4f52374728b76c9118dee03a00
Paul Overbeck <poverbec@3-F-2.42heilbronn.de>
2025-03-20 09:41:58 +0100

    merge

--- philo/src/destroy_mutex.c
+++ philo/src/destroy_mutex.c
@@ -6,7 +6,7 @@
 /*   By: poverbec <poverbec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:24:48 by poverbec          #+#    #+#             */
-/*   Updated: 2025/03/17 16:25:04 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/03/17 16:47:36 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void destro_mutex(t_program *program)
 {
-	
-
+	pthread_mutex_destroy(&program->meal_mutex);
+	pthread_mutex_destroy(&program->must_eat_mutex);
+	pthread_mutex_destroy(&program->print_mutex);
+	pthread_mutex_destroy(&program->death_mutex);
 }
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: poverbec <poverbec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:25:13 by poverbec          #+#    #+#             */
-/*   Updated: 2025/03/17 16:36:11 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/03/17 16:49:16 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int main(int argc, char **argv)
 
     check_arg(argc, argv);
     init_programm(&program, argv, argc); // pthread_mutex_init(&mutex, Null)
-
+	init_philo(&program);
     // pthread_create();
         //pthread_mutex_lock();
         // operation
         // pthread_mutex_unlock()
+	// pthread_join(&mutex)
 	dextroy_mutex(&program);
-    // pthread_join(&mutex)
    // pthread_mutex_destroy(&mutex);
 }
 
-
+init_philo
 // can not pick up fork at the same time
 //pick_up_fork
 // only available forks
