1dfec3b2476a06b1ffa6ca8307f9588ad4a8346c
Paul Overbeck <poverbec@3-F-3.42heilbronn.de>
2025-04-07 13:45:01 +0200

    input'

--- philo/src/check_arg.c
+++ philo/src/check_arg.c
@@ -6,7 +6,7 @@
 /*   By: poverbec <poverbec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:24:43 by poverbec          #+#    #+#             */
-/*   Updated: 2025/04/07 11:23:58 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/04/07 13:43:50 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ bool	check_for_clean_nbr(int argc, char **argv)
 		return (false);
 	if (ft_atoi(argv[3]) == -1)
 		return (false);
-	if (argc == 5)
-		if (ft_atoi(argv[4]) == -1)
+	if (ft_atoi(argv[4]) == -1)
+		return (false);
+	if (argc == 6)
+		if (ft_atoi(argv[5]) == -1)
 			return (false);
 	return (true);
 }
@@ -35,17 +37,20 @@ int	error_input(char *error_msg)
 
 bool	check_arg(int argc, char **argv)
 {
-	if ((argc < 4) && (argc > 5) == false)
+	if ((argc < 5) || (argc > 6) == true)
 	{
 		error_input("Wrong number of arguments");
 		return (false);
 	}
-	if (!argv[1])
+	if (ft_atoi(argv[1]) == 0)
 	{
 		error_input("Number of philos not specified");
 		return (false);
 	}
 	if ((check_for_clean_nbr(argc, argv)) == false)
+	{
+		error_input("Only numerical input is valid");
 		return (false);
+	}
 	return (true);
 }
